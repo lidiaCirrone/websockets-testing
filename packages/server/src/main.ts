24c38f88bda4725384ba8f57e3393b88975b1123
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-15 17:16:54 +0100

    added the websocket server to node

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,1 +1,12 @@
+import { ServerOptions } from 'ws';
+import { WsHandler } from './app/ws-handler';
+
 console.log('Hello World!');
+
+function main() {
+  const options: ServerOptions = { port: 8080 };
+  const handler = new WsHandler();
+  handler.initialize(options);
+}
+
+main();
