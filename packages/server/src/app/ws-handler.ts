6d41e1bf29e22c0e77abbc3f5fee23c16bf2e4cf
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-16 11:34:46 +0100

    created UserManager class to separate connections from messages

--- packages/server/src/app/ws-handler.ts
+++ packages/server/src/app/ws-handler.ts
@@ -1,11 +1,14 @@
 import { IncomingMessage } from 'http';
 import { WebSocket, WebSocketServer, ServerOptions, RawData } from 'ws';
+import { UserManager } from './user-manager';
 
 export class WsHandler {
   private wsServer: WebSocketServer;
+  private userManager: UserManager;
 
   initialize(options: ServerOptions) {
     this.wsServer = new WebSocketServer(options);
+    this.userManager = new UserManager();
 
     this.wsServer.on('listening', () =>
       console.log(`Server listening on port ${options.port}`)
@@ -17,20 +20,23 @@ export class WsHandler {
 
   onSocketConnected(socket: WebSocket, request: IncomingMessage) {
     console.log('New websocket connection!');
+    this.userManager.add(socket);
 
     socket.on('message', (data) => this.onSocketMessage(socket, data));
-    socket.on('close', (code, reason) => this.onSocketClosed(code, reason));
+    socket.on('close', (code, reason) =>
+      this.onSocketClosed(socket, code, reason)
+    );
   }
 
   onSocketMessage(socket: WebSocket, data: RawData) {
     const payload = JSON.parse(`${data}`);
     console.log('Received: ', payload);
 
-    const reply = JSON.stringify({ reply: 'Message received' });
-    socket.send(reply);
+    this.userManager.send(socket, { reply: 'Message received' });
   }
 
-  onSocketClosed(code: number, reason: Buffer) {
+  onSocketClosed(socket: WebSocket, code: number, reason: Buffer) {
     console.log(`Client has disconnected; code=${code}, reason=${reason}`);
+    this.userManager.remove(socket);
   }
 }
