fbc88858cbcff2f3fcfb8f7e352f593d696152b1
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-16 17:10:17 +0100

    set up the chat component

--- packages/webapp/src/app/app.component.ts
+++ packages/webapp/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatRelayMessage, User } from '@websocket/types';
 
 @Component({
   selector: 'websocket-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp';
+
+  messages: ChatRelayMessage[] = [];
+  currentUser: User;
+
+  ngOnInit(): void {
+    this.messages = [
+      {
+        event: 'chatRelay',
+        author: { name: 'Jane', id: 1 },
+        contents: 'Hi, this is Jane',
+      },
+      {
+        event: 'chatRelay',
+        author: { name: 'Henry', id: 2 },
+        contents: 'Hello Jane, I am Henry',
+      },
+    ];
+
+    this.currentUser = { name: 'Xavier', id: 3 };
+  }
 }
